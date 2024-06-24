511509c35935902b06f4b790c1d40bf08eca05b6
hyewon <hyeewooon@naver.com>
2024-06-25 06:46:46 +0900

    feat: 프로젝트 세팅

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    tsconfigRootDir: __dirname,
+    project: 'tsconfig.json',
+  },
+  ignorePatterns: [
+    '*.(test|spec).(ts|tsx)',
+    '*.config.js',
+    '*.config.ts',
+    '.eslintrc.js',
+    'tsconfig.json',
+    '*.md',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+};
