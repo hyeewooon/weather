82c3bb55061eb8024e027ede91191569348e1c20
hyeewooon <hyeewooon@naver.com>
2024-07-01 17:54:16 +0900

    fix: id 있는 경우만 활성화 처리

--- src/app/_queries/useGetForecastInfo.ts
+++ src/app/_queries/useGetForecastInfo.ts
@@ -1,8 +1,10 @@
 import { useQuery, type UseQueryOptions } from '@tanstack/react-query';
 
 import { getForecastInfo } from '../_api/weather';
+import { ForecastResponse } from '../_api/weather/model';
 
 type LocationInfo = {
+  id: string;
   cityName: string;
   lat: number;
   lng: number;
@@ -13,7 +15,10 @@ const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
 function useGetForecastInfo(
   params: LocationInfo,
-  queryOptions?: Omit<UseQueryOptions, 'queryKey' | 'queryFn'>,
+  queryOptions?: Omit<
+    UseQueryOptions<unknown, Error, ForecastResponse, string[]>,
+    'queryKey' | 'queryFn' | 'enabled'
+  >,
 ) {
   const query = useQuery({
     queryKey: ['getForecast', params.cityName],
@@ -24,6 +29,7 @@ function useGetForecastInfo(
         days: params.days,
         lang: 'ko',
       }),
+    enabled: params.id !== '',
     ...queryOptions,
   });
 
