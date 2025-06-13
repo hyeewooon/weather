8affb828594f393a3c2655ec80494053ad8fc3d2
hyeewooon <hyeewooon@naver.com>
2025-06-13 11:40:06 +0900

    chore: 라이브러리 버전 업그레이드

--- src/app/main/_components/currentInfo/dropdown.tsx
+++ src/app/main/_components/currentInfo/dropdown.tsx
@@ -5,22 +5,30 @@ export default function Dropdown() {
   const { isOpen, isLoading, location, locationList, changeOpen, changeLocation } =
     useDropdown();
 
+  const styles /*tw*/ = {
+    container: 'relative',
+    button: 'flex items-center',
+    title: 'min-w-6 mr-2',
+    list: 'flex-none p-3 rounded-md bg-white absolute',
+    item: 'pb-1 cursor-pointer hover:text-gray-600',
+  };
+
   return (
-    <div className="relative">
-      <button className="flex items-center" onClick={changeOpen}>
-        <p className="min-w-6 mr-2">
+    <div className={styles.container}>
+      <button className={styles.button} onClick={changeOpen}>
+        <p className={styles.title}>
           {isLoading ? '~' : location?.cityName ?? '지역을 선택해주세요.'}
         </p>
-        <Image src="/images/ico_arrow_down_24.svg" alt="∨" width={16} height={16} />
+        <Image src="/images/ico_arrow_down_24.svg" alt="arrow" width={16} height={16} />
       </button>
 
       {isOpen && (
-        <ul className="flex-none p-3 rounded-md bg-white absolute">
+        <ul className={styles.list}>
           {locationList.map((loc) => {
             return (
               <li
                 key={loc.id}
-                className="pb-1 cursor-pointer hover:text-gray-600"
+                className={styles.item}
                 onClick={() => changeLocation(loc)}
               >
                 {loc.cityName}
